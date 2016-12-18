62d53cddf994f924f2da7a6a0d339d5b5cc0e6e8
Akira MATSUDA <akira.m.itachi@gmail.com>
2016-12-18 18:15:41 +0900

    Update convert.go
    並列化

--- convert.go
+++ convert.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -10,19 +11,29 @@ import (
 )
 
 func main() {
-	files, _ := ioutil.ReadDir("./")
-	for _, f := range files {
-		file_path := f.Name()
-		ext := filepath.Ext(file_path)
-		if ext == ".png" {
-			s := strings.Split(file_path, ".")
-			filename := s[0]
-			fmt.Println(filename)
-			exec.Command("convert", filename+".png", filename+".jpeg").Run()
-			exec.Command("convert", filename+".jpeg", "eps2:"+filename+".eps").Run()
-			if err := os.Remove(filename+".jpeg"); err != nil {
-				fmt.Println(err)
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		var wg sync.WaitGroup
+		for _, f := range files {
+			file_path := f.Name()
+			ext := filepath.Ext(file_path)
+			if ext == ".png" {
+				s := strings.Split(file_path, ".")
+				filename := s[0]
+				fmt.Println(filename)
+				wg.Add(1)
+				go func() {
+					exec.Command("convert", filename+".png", filename+".jpeg").Run()
+					exec.Command("convert", filename+".jpeg", "eps2:"+filename+".eps").Run()
+					if err := os.Remove(filename+".jpeg"); err != nil {
+						fmt.Println(err)
+					}
+					wg.Done()
+				}()
 			}
 		}
+		wg.Wait()
 	}
 }
